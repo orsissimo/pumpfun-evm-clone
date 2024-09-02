ccda6d1fbe34dd0f8a7d92cedb0346f06a4ce0f2
Simone Orsi <88100810+orsissimo@users.noreply.github.com>
2024-09-02 17:59:28 +0200

    Up create token page, connect wallet and create token function

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>{/* You can include meta tags, link tags, etc., here */}</head>
+      <body className={inter.className}>
+        <Navbar />
+        <ToastContainer />
+        {children}
+      </body>
     </html>
   );
 }
