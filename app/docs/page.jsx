517a99e801b46e3b80920ef0680c3de4c65e8e71
Simone Orsi <88100810+orsissimo@users.noreply.github.com>
2024-09-08 02:33:47 +0200

    Add docs

---
+++ app/docs/page.jsx
@@ -0,0 +1,7 @@
+import { CreateToken } from "@/components/create-token";
+import { Docs } from "@/components/docs";
+import Image from "next/image";
+
+export default function Home() {
+  return <Docs />;
+}
