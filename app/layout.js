bb7f4378d6856ddd9b7dae1be1ed7e1c85c3fc47
Simone Orsi <88100810+orsissimo@users.noreply.github.com>
2024-09-08 15:38:53 +0200

    Update toast style

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
-import { ToastContainer, toast } from "react-toastify";
+import {
+  Bounce,
+  Flip,
+  Slide,
+  ToastContainer,
+  Zoom,
+  toast,
+} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Footer } from "@/components/footer";
 
@@ -19,7 +26,12 @@ export default function RootLayout({ children }) {
       <head>{/* You can include meta tags, link tags, etc., here */}</head>
       <body className={inter.className}>
         <Navbar />
-        <ToastContainer />
+        <ToastContainer
+          position="bottom-right"
+          closeOnClick
+          theme="dark"
+          transition={Bounce}
+        />
         {children}
         <Footer />
       </body>
